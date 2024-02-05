9d57171db6d0b8c0f6b026397cbfad9f3fbe7987
Johndayll Arizala <johndayll_arizala@dlsu.edu.ph>
2024-02-05 12:21:24 +0800

    Add failing test for updating posts

--- tests/post.test.js
+++ tests/post.test.js
@@ -75,6 +75,68 @@ describe('Post controller', () => {
     });
 
     describe('update', () => {
+        var updatePostStub;
+
+        //edited content
+        const post_id = "507asdghajsdhjgas";
+        var req = {
+            body: {
+                author: 'stswenguser',
+                title: 'My first test post (Edited)',
+                content: 'Not Random content'
+            },
+            post_id: post_id
+        };
+
+        beforeEach(() => {
+            // before every test case setup first
+            res = {
+                json: sinon.spy(),
+                status: sinon.stub().returns({ end: sinon.spy() })
+            };
+        });
+
+        afterEach(() => {
+            // executed after the test case
+            updatePostStub.restore();
+        });
+
+        it('should return the updated post object', () => {
+            expectedResult = {
+                _id: post_id,
+                title: 'My first test post (Edited)',
+                content: 'Not Random content',
+                author: 'stswenguser',
+                date: Date.now()
+            };
+
+            updatePostStub = sinon.stub(PostModel, 'updatePost').yields(null, expectedResult);
+
+
+            //Run the post controller
+            PostController.update(req, res);
+            
+            //Make sure post_id and req.body is passed
+            sinon.assert.calledWith(PostModel.updatePost, post_id, req.body);
+
+            //Make sure that the result matches the request
+            sinon.assert.calledWith(res.json, sinon.match({ title: req.body.title }));
+            sinon.assert.calledWith(res.json, sinon.match({ content: req.body.content }));
+            sinon.assert.calledWith(res.json, sinon.match({ author: req.body.author }));
+        });
+
+        it('should return status 500 on server error', () => {
+            updatePostStub = sinon.stub(PostModel, 'updatePost').yields(error);
+
+            PostController.update(req, res);
+
+            //Make sure post_id and req.body is passed
+            sinon.assert.calledWith(PostModel.updatePost, post_id, req.body);
+            
+            //Make sure that an error was raised
+            sinon.assert.calledWith(res.status, 500);
+            sinon.assert.calledOnce(res.status(500).end);
+        });
     });
 
     describe('findPost', () => {
